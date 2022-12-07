4803b1885ddb8004e570c1579afecb208d704eef
ErrorGamer2000 <errorgamer2000@outlook.com>
2022-12-07 16:16:06 -0600

    Code-splitting

--- bundler/rollup.config.js
+++ bundler/rollup.config.js
@@ -2,41 +2,31 @@ import fs from "fs";
 import { addonTransformer } from "./scripts/hmr/rollup-plugins.js";
 
 /** @type {string[]} */
-const addonJSON = JSON.parse(fs.readFileSync("./addons/addons.json", "utf-8"));
+const addons = JSON.parse(fs.readFileSync("addons/addons.json", "utf-8")).filter((v) => !v.startsWith("//"));
+const manifests = Object.fromEntries(
+  addons.map((addonId) => [addonId, JSON.parse(fs.readFileSync(`addons/${addonId}/addon.json`, "utf-8"))])
+);
+
 /**
  * @type {import("rollup").RollupOptions}
  */
 export default [
   {
-    input: {
-      "custom-zoom/userscript.js": "./addons/custom-zoom/userscript.js",
-    },
+    input: Object.fromEntries(
+      Object.entries(manifests)
+        .map(([addonId, manifest]) => {
+          if (!("userscripts" in manifest) || manifest.userscripts.length === 0) return false;
+          return manifest.userscripts.map(({ url }) => [`${addonId}/${url}`, `addons/${addonId}/${url}`]);
+        })
+        .filter(Boolean)
+        .flat()
+    ),
     output: {
       dir: "build/addons",
-      format: "iife",
+      format: "es",
       sourcemap: true,
       entryFileNames: "[name]",
     },
     plugins: [addonTransformer()],
-  } /*,
-  ...addonJSON
-    .filter((v) => !v.startsWith("//"))
-    .map((addon) => {
-      const addonJSON = JSON.parse(fs.readFileSync(`./addons/${addon}/addon.json`));
-      return (addonJSON.userscripts || []).map(({ url }) => {
-        /**
-         * @type {import("rollup").RollupOptions}
-         */ /*
-        const config = {
-          input: `./addons/${addon}/${url}`,
-          output: {
-            file: `./build/addons/${addon}/${url}`,
-            format: "iife",
-            sourcemap: true,
-          },
-          plugins: [addonTransformer(addon, url)],
-        };
-        return config;
-      });
-    }),*/,
+  },
 ];
--- content-scripts/inject/import-addon.js
+++ content-scripts/inject/import-addon.js
@@ -1,8 +1,10 @@
 export function importAddon(addonId, scriptPath) {
   const scriptUrl = `${new URL(import.meta.url).origin}/build/addons/${addonId}/${scriptPath}`;
-  const elem = document.createElement("script");
-  elem.src = scriptUrl;
-  elem.defer = true;
+  const elem = Object.assign(document.createElement("script"), {
+    src: scriptUrl,
+    defer: true,
+    type: "module",
+  });
   return new Promise((resolve, reject) => {
     elem.addEventListener("load", () => {
       elem.remove();
